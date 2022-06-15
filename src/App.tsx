a9795aedbc065e91932d3faa3aaea0c15197de3f
LeonyMalasanManalili <erlyon15@gmail.com>
2022-06-15 03:55:49 +0200

    component Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,52 @@
-import React from "react";
+//importa in app.tsx react e i due metodi dalla libreria react.
+import React, {useState,useEffect} from "react";
 import logo from "./logo.svg";
 //Importazione della pagina di stile per questo applicativo.
 import "./App.css";
+//importa i component che dovranno essere inseriti(creo i componenti in una cartella appostita)
+import Card from "./components/CARD/Card";
+// import Button from "./components/BUTTON/Button";
+import { textChangeRangeIsUnchanged } from "typescript";
+
+//Per prima cosa creo l'interfaccia per l'api a cui mi collegherò e utilizzerò.
+//Andrà creata a seconda della struttura dell'api e essendo un oggetto literal lo strutturerò in questo modo
+//tipizzando le chiavi e dandogli valore null iniziale.
+export interface CardAdvice{
+  text: string|null,
+  id: number|null
+}
+
 
 //Creazione funzione da importare ovvero oggetto da mettere nell'index.
 function AdviceGenerator() {
+  //Adesso posso collegarmi all'Api
+  //Creo una funzione seguita dalle quadre a cui all'interno setto il primo parametro ovverlo stato e nel secondo cosa può far cambiare lo stato
+  //in questo modo = useState dell'interfaccia CardAdvice saranno allo stato iniziale null.
+  const [advice,setAdvice] = useState<CardAdvice>({text:null,id:null})
+  //Creo un secondo stato per il bottone
+  const [clickhere,setClickhere] = useState<number>(1)
+  //Andrò dopo settato gli useState() a utilizzare il metodo useEffect() per il collegamento all'api
+  useEffect(()=>{
+    fetch("https://api.adviceslip.com/advice")
+    //una volta fetchato utilizzo il then per poter in caso risulatasse resolve 
+    .then(resolve => resolve.json())
+    .then(Dati => {
+      setAdvice({
+        text: Dati.slip.advice,
+        id: Dati.slip.id
+      })
+    })
+    },[clickhere])
   //questa funziona mi restituisce questo HTML con Jsx
-  return (
-  <div className="Advice-container">
-  <CARD \>
-  <BUTTON \>
-  </div> 
-  )
-
+  return <div className="Advice-container">
+    <Card advice={advice} />
+  </div>;
 }
 
+//Adesso pocco creare i componenti exportando l'interfaccia rendendola globale e richiamabile
+
 export default AdviceGenerator;
+function then(arg0: (resolve: any) => any) {
+  throw new Error("Function not implemented.");
+}
+
--- src/components/CARD/Card.tsx
+++ src/components/CARD/Card.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import "./Card.modules.css"
+import React, { useState, useEffect } from "react";
+//mi importo l'interfaccia
+import { CardAdvice } from "../../App";
+//collego il foglio di stile ,importandoli tutti da qui
+import styles from "./Card.module.css";
+//Importo l'immagine creando const divider che e parte del componente Card che realizzerò 
+import Divisore from "../../assets/pattern-divider-desktop.svg";
 
-const Card =()=>{
-    return(
-        <div className="elementCards">
-            
-        </div>
-    )
-} 
+//Creo un iterfaccio che mi prenderà al suo interno , l'interfaccia CardAdvice importata.
+interface Cardstructure {
+  advice: CardAdvice;
+}
+
+const Card: React.FC<Cardstructure> = ({ advice }) => {
+  return (
+    //Assegno tutti gli stili importati in con il dot. per ogni classe
+    //con jsx riesco a inserire la proprieta advice con valore id
+    <div className={styles.Card}>
+      <h3 className={styles.advice}> ADVICE # {advice.id}</h3>
+      <h2 className={styles.text}>"{advice.text}"</h2>
+      <img src={Divisore} alt="Imagginedivisore" className={styles.cardImg} />
+    </div>
+  );
+};
+
+//exporto per renderlo globale e richiamabile
+export default Card;
